fc66aade0a4aeea73b3d88088b6f239a4b4fb2ba
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-06-19 18:10:17 +0000

    Adding functionality to update array on enter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React ,{ useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState(null);
+	console.log(inputValue);
+
+	const [todoArray, setTodoArray] = useState([]);
+	console.log("Todo Array:", todoArray);
+
+	const handleKeyDown = (event) => {
+		console.log(event.key);
+		if (event.key === "Enter"){
+			setTodoArray(previousState => {
+				return previousState.concat(inputValue);
+			})
+		};
+	
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1 className="text-center">todos</h1>
+			<div className="d-flex justify-content-center">
+				<ul className="list-group w-50">
+					<input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={handleKeyDown} placeholder="What needs to be done?"/>
+					<li className="list-group-item">{todoArray[0]}</li>
+					<li className="list-group-item">{todoArray[1]}</li>
+					<li className="list-group-item">{todoArray[2]}</li>
+					<li className="list-group-item">{todoArray[3]}</li>
+					<li className="list-group-item">{todoArray[4]}</li>
+				</ul>
+			</div>
 		</div>
-	);
+	)
 };
 
 export default Home;
