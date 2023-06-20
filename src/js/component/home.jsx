0059a5f2dba8fdad10bfa49ab4867e339d19e609
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-06-20 09:35:51 +0000

    Adding button functionality to remove list items on click

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,6 +21,13 @@ const Home = () => {
 		};	
 	};
 
+	const removeItem = (index) => {
+		console.log(todoArray, index);
+		//todoArray.splice(index, 1) does not work to manipulate the state
+		const updatedTodo = todoArray.filter((_, i) => i !== index);
+		setTodoArray(updatedTodo);
+	}
+
 	return (
 		<div>
 			<h1 className="text-center">todos</h1>
@@ -28,7 +35,7 @@ const Home = () => {
 				<ul className="list-group w-50">
 					<input className="list-style" type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={handleKeyDown} placeholder="What needs to be done?"/>
 					{todoArray.map( (item, index) => {
-						return <li className="list-group-item list-style" key={index}>{item}<button>x</button></li>
+						return <li className="list-group-item list-style" key={index}>{item} <button onClick={() => removeItem(index)}>x</button> </li>
 					})
 					}
 					<li className="list-group-item list-style task-counter">{todoArray.length} items left</li>
