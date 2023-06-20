3eb64c1e243b8135649b4e8fd1d158fd5ea32c41
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-06-20 13:05:54 +0000

    Adding -No tasks, add a task- functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,15 +8,20 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState(null);
 	console.log(inputValue);
 
-	const [todoArray, setTodoArray] = useState([]);
+	const [todoArray, setTodoArray] = useState(["No tasks, add a task"]);
 	console.log("Todo Array:", todoArray);
 
 	const handleKeyDown = (event) => {
 		console.log(event.key);
 		if (event.key === "Enter"){
-			setTodoArray(previousState => {
-				return previousState.concat(inputValue);
-			})
+			todoArray[0] === "No tasks, add a task"
+				? setTodoArray(todoArray.concat(inputValue).slice(1,todoArray.length+1))
+				: setTodoArray(todoArray.concat(inputValue))
+			
+			// // Same as above (without checking first array item)
+			// setTodoArray(previousState => {
+			// 	return previousState.concat(inputValue);
+			// })
 			setInputValue("");
 		};	
 	};
@@ -24,6 +29,7 @@ const Home = () => {
 	const removeItem = (index) => {
 		console.log(todoArray, index);
 		//todoArray.splice(index, 1) does not work to manipulate the state
+		// need to use Filter - to filer out (=return) values 
 		const updatedTodo = todoArray.filter((_, i) => i !== index);
 		setTodoArray(updatedTodo);
 	}
